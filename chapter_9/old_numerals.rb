470b0c14909bba4ba1cf323006f05c5c1f800d28
robincollins <racollins1@me.com>
2017-08-06 15:21:40 +0100

    update

--- chapter_9/old_numerals.rb
+++ chapter_9/old_numerals.rb
@@ -1,25 +1,48 @@
+def old_numeral num
+  roman = ""
+
+  roman = roman + "M" * (num        / 1000)
+  roman = roman + "D" * (num % 1000 /  500)
+  roman = roman + "C" * (num % 500  /  100)
+  roman = roman + "L" * (num % 100  /   50)
+  roman = roman + "X" * (num % 50   /   10)
+  roman = roman + "V" * (num % 10   /    5)
+  roman = roman + "I" * (num % 5    /    1)
+  roman
+end
+
+puts(old_numeral(105))
+
+
+
+
+
+
 # integer division (/) and modulus methods (%)
+# pg 80
 
-puts "Give me an integer, and I will convert to"
-puts "old-school Roman numeral:"
+#puts "Give me an integer, and I will convert to"
+#puts "old-school Roman numeral:"
 
-input = gets.chomp#to_i / to_s?
+#input = gets.chomp#to_i / to_s?
 
-input = number
+#input = number
+
 #method that converts number into corresponding
-#numrals...
+#numerals...
 
+#def
+
+#end
 # if number is 4 digits, divide by 1000 etc??
 # if number is 3 digits, divide by 500? Then 100?
 
 # use a hash?!
 
-{"I" = 1}
-{"V" = 5}
-{"X" = 10}
-{"L" = 50}
-{"C" = 100}
-{"D" = 500}
-{"M" = 1000}
-
-def
+#numeral["I"] = 1
+#numeral["V"] = 5
+#numeral["X"] = 10
+#numeral["L"] = 50
+#numeral["C"] = 100
+#numeral["D"] = 500
+#numeral["M"] = 1000
