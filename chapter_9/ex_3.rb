2ff45e14643af876092cf2adafe7e71a7f6ec360
robincollins <racollins1@me.com>
2017-08-06 12:40:36 +0100

    ex_3

---
+++ chapter_9/ex_3.rb
@@ -0,0 +1,11 @@
+def double_this num
+  num_times_2 = num*2
+  puts "#{num.to_s} doubled is #{num_times_2.to_s}"
+end
+
+double_this 44
+
+#num and num_times_2 are BOTH vairables, which sit
+#inside the method double_this
+#They are LOCAL variables, meaning they live
+#inside the method!
