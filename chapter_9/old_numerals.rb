5e1f308a6fc0646c8b360dda47b7e444c83dd3f5
robincollins <racollins1@me.com>
2017-08-06 14:32:00 +0100

    update

---
+++ chapter_9/old_numerals.rb
@@ -0,0 +1,25 @@
+# integer division (/) and modulus methods (%)
+
+puts "Give me an integer, and I will convert to"
+puts "old-school Roman numeral:"
+
+input = gets.chomp#to_i / to_s?
+
+input = number
+#method that converts number into corresponding
+#numrals...
+
+# if number is 4 digits, divide by 1000 etc??
+# if number is 3 digits, divide by 500? Then 100?
+
+# use a hash?!
+
+{"I" = 1}
+{"V" = 5}
+{"X" = 10}
+{"L" = 50}
+{"C" = 100}
+{"D" = 500}
+{"M" = 1000}
+
+def
