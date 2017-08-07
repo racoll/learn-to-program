26c98abf6f2a576fb1bbc2301f20f638f752b6b3
robincollins <racollins1@me.com>
2017-08-07 14:29:56 +0100

    total update

---
+++ chapter_11/ex_4.rb
@@ -0,0 +1,27 @@
+require 'yaml'
+# first we define these methods
+def yaml_save object, filename
+  File.open filename, "w" do |f|
+    f.write(object.to_yaml)
+  end
+end
+
+def yaml_load filename
+  yaml_string = File.read filename
+
+  YAML::load yaml_string
+end
+# and now we use the methods...
+test_array = ["Slick Shoes",
+              "Bully Blinders",
+              "Pinchers of Peril"]
+
+filename = "DatasGadgets.txt"
+
+# we save it
+yaml_save test_array, filename
+
+# we load it
+read_array = yaml_load filename
+
+puts(read_array == test_array)
