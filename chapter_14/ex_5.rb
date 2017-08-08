696c861c9a1c0858c2c0e921b5f02b53cb3bf0a5
robincollins <racollins1@me.com>
2017-08-08 22:05:46 +0100

    save update

---
+++ chapter_14/ex_5.rb
@@ -0,0 +1,34 @@
+def do_until_false first_input, some_proc
+  input = first_input
+  output = first_input
+
+  while output
+    input = output
+    output = some_proc.call input
+  end
+
+  input
+end
+
+build_array_of_squares = Proc.new do |array|
+  last_number = array.last
+  if last_number <= 0
+    false
+  else
+    # take off the last number
+    array.pop
+    # and replace it with its square (0 squared is still 0)
+    array.push last_number * last_number
+    # followed by the next smaller number
+    array.push last_number - 1
+  end
+end
+
+always_false = Proc.new do |just_ignore_me|
+  false
+end
+
+puts do_until_false([5], build_array_of_squares).inspect
+
+yum = "lemonade with a hint of orange blossom water"
+puts do_until_false(yum, always_false)
